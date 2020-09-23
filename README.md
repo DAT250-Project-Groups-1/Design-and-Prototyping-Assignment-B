86adb50a18f00a53b9e6f138239c34e0aa8ac6ff
andreasgarvik <andreas.garvik13@gmail.com>
2020-09-23 18:41:32 +0200

    :rocket: Run script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Design-and-Prototyping-Assignment-B
+
+Voting app :rocket:
+
+Start with `./run.sh`
---
+++ run.sh
@@ -0,0 +1,3 @@
+chmod +x run.sh;
+./gradlew build; 
+java -jar build/libs/design-and-prototyping-assignment-b-0.0.1-SNAPSHOT.jar
