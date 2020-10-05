0d7372d71d525fa1828ae26b50f46ecb68bfedad
Martin Kydland <martin.kydland@student.uib.no>
2020-10-05 15:51:39 +0200

    test

--- src/main/java/com/dat250/feedapp/controllers/IOTController.java
+++ src/main/java/com/dat250/feedapp/controllers/IOTController.java
@@ -1,4 +1,32 @@
 package com.dat250.feedapp.controllers;
 
+import com.dat250.feedapp.models.Vote;
+import com.dat250.feedapp.services.VotingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
 public class IOTController {
+
+    @Autowired
+    private IOTController iotController;
+
+    @GetMapping("/iotDevices")
+    List<IOTDevice> getIOTDevices(){
+        return iotController.readAllIOTDevices();
+    }
+
+    @GetMapping("/iotDevices/{id}")
+    IOTDevice getIOTDevice(@PathVariable(name = "id") int id){
+        return IOTDeviceService.readIOTDeviceById(id);
+    }
+
+    @PostMapping("/iotDevices")
+    IOTDevice postIOTDevice(@RequestBody IOTDevice iotDevice){
+
+    }
 }
